d6972b369219f58b5b245e8c63400720df53c444
pouriaabbasi <pouria_abbasi@hotmail.com>
2019-02-04 11:10:12 +0330

    fix

--- ClientApp/src/app/COMPONENTS/types/types.component.ts
+++ ClientApp/src/app/COMPONENTS/types/types.component.ts
@@ -13,6 +13,7 @@ export class TypesComponent implements OnInit {
   private displayedColumns: string[] = ['id', 'name', 'description', 'p2PPlayCount', 'isContinuous', 'actions'];
   private dataSource = new MatTableDataSource();
   private types: TypeModel[] = [];
+  private type: TypeModel = null;
 
   constructor(
     private typeService: TypeService
@@ -32,6 +33,29 @@ export class TypesComponent implements OnInit {
     this.typeService.GetTypes().subscribe(types => {
       this.types = types;
     })
+  }
+
+  private selectType(type: TypeModel) {
+    this.type = type;
+  }
+
+  private newType() {
+    this.type = new TypeModel();
+  }
+
+  private submit() {
+    if (this.type) {
+      if (this.type.id != 0) {
+        //call edit api
+        this.type = null;
+        this.refreshGrid();
+      }
+      if (this.type.id == 0) {
+        //call add api
+        this.type = null;
+        this.refreshGrid();
+      }
+    }
   }
 
 }
